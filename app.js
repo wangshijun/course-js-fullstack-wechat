92a379de8933bade439c43335ee056882c3ffd87
wangshijun2010 <wangshijun2010@gmail.com>
2016-06-13 07:19:31 +0800

    make static assets work

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(XMLParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/wechat/static/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
